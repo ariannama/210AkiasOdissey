f0c489ec05687850858e27714d135147d511d0cc
Marrocu <marrocu@lancaster.ac.uk>
2019-01-31 18:42:26 +0000

    Added instance of RenderWindow and methods to keep it open and only closed when exit button is pressed

--- Menu.java
+++ Menu.java
@@ -1,3 +1,5 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
@@ -8,9 +10,31 @@ import org.jsfml.system.*;
 import org.jsfml.window.*;
 import org.jsfml.window.event.*;
 import org.jsfml.graphics.*;
+
+/**
+ * <h1>Menu</h1>
+ *
+ * The menu class represents the menu of the game.
+ *
+ * @author: Arianna Marrocu
+ */
 public class Menu {
 
     public Menu(){
+        VideoMode vm = new VideoMode(500, 500);
+        RenderWindow w = new RenderWindow(vm, "Akia's Odissey", WindowStyle.DEFAULT);
+        w.create(vm, "Akia's Odissey");
+        w.setVisible(true);
+        w.display();
+        while(w.isOpen()){
+            for(Event event :w.pollEvents()){
+                if(event.type == Event.Type.CLOSED){
+                    w.close();
+                }
+            }
+        }
 
     }
+
+
 }
