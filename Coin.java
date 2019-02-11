a0eeb0cbea23e19616abd6e015e64f81d86e808c
MichaelHeaver <46893532+MichaelHeaver@users.noreply.github.com>
2019-02-11 14:26:56 +0000

    Add files via upload

--- Coin.java
+++ Coin.java
@@ -2,13 +2,17 @@ import org.jsfml.graphics.Sprite;
 import org.jsfml.graphics.Texture;
 
 /*
-  * <h1>Coin</h1>
-  *
-  *  This is the coin class
-  *
-  *  @Author Michael Heaver
-*/
+ * <h1>Coin</h1>
+ *
+ *  This is the coin class
+ *
+ *  @Author Michael Heaver
+ */
 public class Coin extends Collectibles {
+
+    public void pickUp(){
+        despawnCollectible();
+    }
 
     public Coin(Sprite coin, Texture coinTexture,  float x, float y){
         super(coin, coinTexture,"coin");
