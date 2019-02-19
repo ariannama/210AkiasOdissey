a3f640634f9854adb6746bf7673f35003ea8d184
moseslynden <mlynden97@outlook.com>
2019-02-19 14:23:13 +0000

    Temp icons for player

--- src/Main.java
+++ src/Main.java
@@ -10,22 +10,44 @@ import java.nio.file.Paths;
 
 public class Main{
     public static void main (String[] args) {
-        int screenWidth  = 1366;
+        int screenWidth  = 1024;
         int screenHeight = 768;
         RenderWindow window = new RenderWindow(new VideoMode(screenWidth, screenHeight), "Akia's Oddysey", WindowStyle.CLOSE);
-        Texture backGround = new Texture();
+
+        Texture imgTexture = new Texture();
+        Texture back = new Texture();
+        Texture leftTexture = new Texture();
+        Texture rightTexture = new Texture();
+        Texture backTexture = new Texture();
+
         try {
-            //playerTexture.loadFromFile(Paths.get("E:\\The Folder\\Game\\front.png"));
-            backGround.loadFromFile(Paths.get("E:\\The Folder\\Game\\BG.png"));
+            imgTexture.loadFromFile(Paths.get("C:\\Users\\mlynd\\Desktop\\210 Game\\front.png"));
+            leftTexture.loadFromFile(Paths.get("C:\\Users\\mlynd\\Desktop\\210 Game\\left.png"));
+            rightTexture.loadFromFile(Paths.get("C:\\Users\\mlynd\\Desktop\\210 Game\\right.png"));
+            backTexture.loadFromFile(Paths.get("C:\\Users\\mlynd\\Desktop\\210 Game\\back.png"));
+            back.loadFromFile(Paths.get("C:\\Users\\mlynd\\Desktop\\210 Game\\BG.png"));
             System.out.println("--success--");
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+        System.out.println(back.getSize());
+        imgTexture.setSmooth(true);
+        Sprite img = new Sprite(imgTexture);
+        double i = 0.8;
 
-        Sprite background = new Sprite(backGround);
+        img.setScale((float)i ,(float)i );
+        img.setPosition(screenWidth/2, 0);
 
 
 
+        Sprite background = new Sprite(back);
+        System.out.println("SCALE: " + background.getScale());
+        background.setScale((float) 0.75, 1);
+
+        System.out.println(imgTexture.getSize());
+        System.out.println(img);
+        Player p = new Player(img, "Akia", 5, 1, 5, 5, 5,5);
+        window.setFramerateLimit(200);
         while (window.isOpen()) {
             for (Event e : window.pollEvents()) {
             }
@@ -33,11 +55,12 @@ public class Main{
             {
                 window.close();
             }
+            p.calcMove();
+            p.move();
             window.clear();
             window.draw(background);
-            //window.draw(img);
+            window.draw(img);
             window.display();
         }
-
     }
 }
