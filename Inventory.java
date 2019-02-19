2c603f39efca85649a25ac8fc2e5ef029aed86f8
moseslynden <m.lynden@lancaster.ac.uk>
2019-02-19 21:36:50 +0000

    Main class and engine class rewoeked

--- Inventory.java
+++
@@ -1,9 +0,0 @@
-public class Inventory {
-    private int coinTotal,keyTotal;
-    private Weapons currentWeapon;
-    private Scroll currentScroll;
-    public Inventory(){
-        coinTotal=keyTotal=0;
-        //currentWeapon = startingWeapon id of 1;
-    }
-}
---
+++ src/Game.java
@@ -0,0 +1,40 @@
+import org.jsfml.graphics.RenderWindow;
+import org.jsfml.system.Vector2f;
+import org.jsfml.system.Vector2i;
+import org.jsfml.window.Mouse;
+import org.jsfml.window.VideoMode;
+import org.jsfml.window.WindowStyle;
+import org.jsfml.window.event.Event;
+
+public class Game {
+    public RenderWindow window;
+    public Menu menu;
+
+    public Game(){
+        initWindow("Akia's Oddysey", 1024, 768);
+        menu = new Menu(window);
+        runMenu();
+    }
+
+    public void initWindow(String title, int width, int height){
+        window = new RenderWindow(new VideoMode(width,height), title, WindowStyle.CLOSE);
+    }
+
+    public void runMenu(){
+        while (window.isOpen()){
+            for(Event e : window.pollEvents()){
+                Vector2i mousePos2i = Mouse.getPosition(window);
+                Vector2f mousePos2f = new Vector2f(mousePos2i);
+                //System.out.println(menu.button1.s.getGlobalBounds());
+                //System.out.println(mousePos2f);
+                //System.out.println(Mouse.getPosition());
+                if((Mouse.isButtonPressed(Mouse.Button.LEFT) == true) && (menu.button1.s.getLocalBounds().contains(mousePos2f)))
+                    System.out.println("Fuck yeah");
+            }
+            for(Entity temp: menu.menuEntity){
+                window.draw(temp.getSprite());
+            }
+            window.display();
+        }
+    }
+}
