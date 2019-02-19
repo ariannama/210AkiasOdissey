1376911bb8a95b7c1213357e23c31df29d66c53f
moseslynden <m.lynden@lancaster.ac.uk>
2019-02-19 21:39:53 +0000

    Final menu class added (For now)

---
+++ src/Button.java
@@ -0,0 +1,9 @@
+import org.jsfml.graphics.RenderWindow;
+import org.jsfml.graphics.Texture;
+
+public class Button extends Entity{
+
+    public Button(Texture t, RenderWindow window){
+        super(t, window);
+    }
+}
