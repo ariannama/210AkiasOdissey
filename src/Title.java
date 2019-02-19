55a3939ff9a4fd875f704c026a0c45c2d96e5a05
moseslynden <m.lynden@lancaster.ac.uk>
2019-02-19 21:39:27 +0000

    Title class added

---
+++ src/Title.java
@@ -0,0 +1,9 @@
+import org.jsfml.graphics.RenderWindow;
+import org.jsfml.graphics.Texture;
+
+public class Title extends Entity{
+
+    public Title(Texture t, RenderWindow window){
+        super(t, window);
+    }
+}
