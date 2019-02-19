7681a27fef03ee34c126c20d24b06f383a95bc00
moseslynden <m.lynden@lancaster.ac.uk>
2019-02-19 21:38:37 +0000

    Breaking up menu into classes, first background

---
+++ src/Background.java
@@ -0,0 +1,9 @@
+import org.jsfml.graphics.RenderWindow;
+import org.jsfml.graphics.Texture;
+
+public class Background extends Entity{
+
+    public Background(Texture t, RenderWindow window){
+        super(t, window);
+    }
+}
