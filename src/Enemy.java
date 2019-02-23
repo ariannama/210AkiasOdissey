319f260abd728a55e7f1b052d43fdc322f158ba7
marrocu <ariannamarrocu55@gmail.com>
2019-02-23 21:37:47 +0000

    New class - extends from character, has an ID. Also has a takeDamage method

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,4 +1,45 @@
-package PACKAGE_NAME;
+import org.jsfml.graphics.Texture;
 
-public class Enemy {
+public class Enemy extends Character {
+        protected final String ID;
+        protected int x;
+        protected int y;
+        protected int range;
+        protected Player player;
+
+        public Enemy(Texture t, String ID, int dmg, int mS, int maxH)
+        {
+            super(t, dmg, mS, maxH);
+            this.ID = ID;
+            this.range = range;
+            //this.player = player;
+        }
+
+        //if PLAYER within SIGHT move within RANGE to ATTACK,
+        //else WANDER(move to random adjacent tile)
+
+        //Enemy can always see player and will move within range to attack
+
+        public void updatePos()
+        {
+            //updates pos of enemys within the arean
+        }
+
+        public void takeDamage() {
+            currentHealth = currentHealth - player.getPlayerDamage();
+           // getSprite().setTexture(Paths.get("../images/");
+            // getSprite().setTexture(texture)
+            if(currentHealth == 0) {
+                getSprite().setPosition(4000,4000);
+            }
+        }
+
+
+        public void attack(){}
+
+        public void move(){}
+
+        public void collision(){
+        }
+
 }
